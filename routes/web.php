3ae7f185df962bafebaac0ba951218b8b1aae3b3
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-10-28 16:17:25 -0400

    feat: criando rota para o controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Login;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login',[Login::class,'Login'])->name('Login');
