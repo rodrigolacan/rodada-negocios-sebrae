31f69e5ecd5f59268958121be239712250e57262
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-10-28 14:14:46 -0400

    feat: criando provider para autenticação LDAP

---
+++ app/Providers/LDAP/AuthService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers\LDAP;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthService extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
