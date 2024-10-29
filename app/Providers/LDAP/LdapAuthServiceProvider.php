fdf11417079f1ae7d2e57d3a56f637895e8cfd04
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-10-29 09:39:10 -0400

    feat: criando pastas para interfaces LDAP

---
+++ app/Providers/LDAP/LdapAuthServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers\LDAP;
+
+use Illuminate\Support\ServiceProvider;
+use App\Contracts\LDAP\LdapContract;
+use App\Services\LdapAuthService;
+
+class LdapAuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app()->singleton(LdapContract::class, function($app){
+            return new LdapAuthService();
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
