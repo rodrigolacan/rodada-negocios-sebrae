466ff64b1a09a974aafef6da9dd5f6e67d1f7183
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-10-30 15:06:04 -0400

    feat: criando rota para receber token jwt do keycloack do AMEI

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Login\LoginAD;
+use App\Http\Controllers\Auth\Amei\AmeiAuthenticator;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
+
+//Rotas de authenticação
+Route::get('auth/amei/{token}', [AmeiAuthenticator::class, 'userAuth'])->name('AuthTokenAmei');
 
 Route::get('/login', [LoginAD::class, 'login'])->name('Login');
 Route::post('/login', [LoginAD::class, 'loginService'])->name('LoginService');
