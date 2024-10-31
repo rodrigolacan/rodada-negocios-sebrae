6c9f96a7fc2a08352a29d16f8adfcaa0a5265e39
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-10-31 12:11:04 -0400

    refact: concertando rotas

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Login\LoginAD;
-use App\Http\Controllers\Auth\Amei\AmeiAuthenticator;
+use App\Http\Middleware\Auth\IsUserLogged;
+use App\Http\Controllers\Auth\Amei\AmeiTokenValidate;
+use App\Http\Controllers\Login\LoginAmei;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('welcome');
+})->name('welcome')->middleware(IsUserLogged::class);
 
+//Rota de authenticação JWT
+Route::get('/auth/amei/token/{token}', [AmeiTokenValidate::class, 'tokenValidate'])->name('AuthTokenAmei');
 
-//Rotas de authenticação
-Route::get('auth/amei/{token}', [AmeiAuthenticator::class, 'userAuth'])->name('AuthTokenAmei');
 
 Route::get('/login', [LoginAD::class, 'login'])->name('Login');
 Route::post('/login', [LoginAD::class, 'loginService'])->name('LoginService');
