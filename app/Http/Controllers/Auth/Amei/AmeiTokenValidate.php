be51a7e1c827222eb72620466d661f9176a328a6
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-10-31 12:10:34 -0400

    feat: adicioandno melhores depurações ao controller e redirecionando para a págian welcome

--- app/Http/Controllers/Auth/Amei/AmeiTokenValidate.php
+++ app/Http/Controllers/Auth/Amei/AmeiTokenValidate.php
@@ -2,21 +2,41 @@
 
 namespace App\Http\Controllers\Auth\Amei;
 
+use App\Contracts\JWT\JwtContract;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cookie;
+
 
 class AmeiTokenValidate extends Controller
 {
+    protected $jwtService;
+    public function __construct(JwtContract $jwtService)
+    {
+        $this->jwtService = $jwtService;
+    }
     public function tokenValidate($token)
     {
         $keyPath = app_path('Keys/AMEI/public_key.pem');
 
-        $publicKeyPEM = file_get_contents($keyPath);
+        try {
+            $tokenDecoded = $this->jwtService->decode($token, $keyPath);
+            // Extrai o tempo de expiração do token
+            $expTime = $tokenDecoded['exp']; // Timestamp Unix
+            $currentTimestamp = time(); // Timestamp atual
 
-        $decoded = JWT::decode($token, new Key($publicKeyPEM, 'RS256'));
-        $decoded_array = (array) $decoded;
-        echo "<pre>" . print_r($decoded_array, true) . "</pre>";
+            // Calcula a diferença entre o tempo de expiração e o timestamp atual
+            $cookieExpiration = $expTime - $currentTimestamp;
+
+            // Define a expiração do cookie com a mesma duração do token
+            Cookie::queue('JTI', strval($tokenDecoded['jti']), $cookieExpiration);
+            Cookie::queue('CID', strval($token), $cookieExpiration);
+
+            return redirect()->route('welcome');
+            return;
+        } catch (\Exception $e) {
+            Log::channel('jwt')->error('Token inválido ou erro ao decodificar (Log do controller): ' . $e->getMessage());
+        }
     }
 }
